6348906648a86c5e785c1a5f8bbcf65eb168cab3
Lucas Grober <grober39@gmail.com>
2020-01-07 18:58:19 -0300

    Init commit

--- source/app.js
+++ source/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 s
-//Setup static directory to serve
+//Setup static directory to serve alou
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) =>{
